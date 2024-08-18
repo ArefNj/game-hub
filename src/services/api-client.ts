9fae14bfddddf1b1cb74a4264c430bbf2fd9d989
ArefNj <Aref.Najmaddini@gmail.com>
2024-08-18 14:37:50 +0330

    display critic score

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    
-    params: {
-        key: 'b826b2e649b54c52b1873a50b07fb585'
-    }
+  baseURL: "https://api.rawg.io/api",
 
-})
+  params: {
+    key: "b826b2e649b54c52b1873a50b07fb585",
+  },
+});

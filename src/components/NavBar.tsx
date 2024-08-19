4f501c71c7364391f2610c6799e04ddd9eb3ac77
ArefNj <Aref.Najmaddini@gmail.com>
2024-08-19 19:14:37 +0330

    build search bar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,12 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/vecteezy_wolf-head-logo-vector-icon-illustration_6640995.jpg";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack padding="10px">
       <Image src={logo} boxSize="60px" borderRadius="15px" />
+      <SearchInput/>
       <ColorModeSwitch />
     </HStack>
   );

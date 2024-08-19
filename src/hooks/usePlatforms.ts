8f9ff57977607ae3177360afcd00857ce0de11ef
ArefNj <Aref.Najmaddini@gmail.com>
2024-08-19 20:31:39 +0330

    comment a parent platform interface

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -6,12 +6,12 @@ export interface Platform {
   slug: string;
 }
 
-interface ParentPlatforms {
-  id: number;
-  name: string;
-  slug: string;
-  Platforms: Platform[];
-}
+// interface ParentPlatforms {
+//   id: number;
+//   name: string;
+//   slug: string;
+//   Platforms: Platform[];
+// }
 
 const usePlatforms = () => ({ data: platforms, error: null, isLoading: false });
 export default usePlatforms;
